b747023299bea8d4d4fd455256991a50c94c7712
Matt Trussell <smtrussell@gmail.com>
2018-11-06 04:37:18 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 
+class Driver{
+  constructor(name, startDate) {
+    this.name = name;
+    this.startDate = new Date(startDate);
+  }
+
+  yearsExperienceFromBeginningOf(year) {
+    return year - this.startDate.getFullYear();
+  }
+}
+
+
+class Route{
+  constructor(beginningLocation, endingLocation) {
+    this.beginningLocation = beginningLocation;
+    this.endingLocation = endingLocation;
+  }
+
+  blocksTravelled() {
+    const eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue'];
+    const horizontalStart = eastWest.indexOf(this.beginningLocation.horizontal);
+    const horizontalEnd = eastWest.indexOf(this.endingLocation.horizontal);
+    const verticalBlocksTravelled = Math.abs(this.beginningLocation.vertical - this.endingLocation.vertical);
+    const horizontalBlocksTravelled = Math.abs(horizontalStart - horizontalEnd);
+    return verticalBlocksTravelled + horizontalBlocksTravelled;
+  }
+
+  estimatedTime(peak = false) {
+    if (peak) {
+      return this.blocksTravelled() / 2;
+    } else {
+      return this.blocksTravelled() / 3;
+    }
+  }
+}
