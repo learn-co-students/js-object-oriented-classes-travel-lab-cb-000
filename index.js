c10399c945b8566965e128d2205109966da57dbf
Konstantinos Pediaditakis <pediadk@gmail.com>
2019-05-22 09:34:43 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
+let eastWest = [
+  '1st Avenue',
+  '2nd Avenue',
+  '3rd Avenue',
+  'Lexington Avenue',
+  'Park',
+  'Madison Avenue',
+  '5th Avenue'
+];
 
+class Driver {
+  constructor(name, startDate) {
+    this.name = name;
+    this.startDate = new Date(startDate);
+  }
+
+  yearsExperienceFromBeginningOf(year) {
+    return year - this.startDate.getFullYear();
+  }
+}
+
+class Route {
+  constructor(beginningLocation, endingLocation) {
+    this.beginningLocation = beginningLocation;
+    this.endingLocation = endingLocation;
+  }
+
+  blocksTravelled() {
+    return Math.abs(parseInt(this.endingLocation.vertical, 10) - parseInt(this.beginningLocation.vertical, 10)) + Math.abs(eastWest.indexOf(this.beginningLocation.horizontal) - eastWest.indexOf(this.endingLocation.horizontal));
+  }
+
+  estimatedTime(peakHours) {
+    return peakHours ? this.blocksTravelled() / 2 : this.blocksTravelled() / 3;
+  }
+
+}
